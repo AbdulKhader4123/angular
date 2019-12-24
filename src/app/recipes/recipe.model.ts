f20ecd3fbdb0b88099b9d62c3760ae7287367197
abdulkhader4123 <abdulkhader4123@gmail.com>
2019-12-24 11:50:30 +0530

    ChangeSet_12_24_2019

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,13 +1,17 @@
+import { Ingredients } from '../shared/Ingredients.model';
+
 export class Recipe{
     public name :string;
     public description:string;
     public imagePath:string;
+    public ingredients:Ingredients[];
 
-    constructor(name :string,description:string,imagePath:string){
+    constructor(name :string,description:string,imagePath:string,ingredients:Ingredients[]){
 
         this.description=description;
         this.name=name;
         this.imagePath=imagePath;
+        this.ingredients=ingredients;
     }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,17 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredients } from '../shared/Ingredients.model';
 
 export class RecipeService{
 
     recipeSelected = new EventEmitter<Recipe>();
    private  recipes :Recipe[]=[
-        new Recipe('A test Recipe','This is an Awesome recipe','https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQoGEyxJ-53jIS2PZMCdfwECFja0ZCYFnrgZlfecyeeSqNybazb')
+        new Recipe('Burger','This is an Awesome recipe','https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/165384.jpg',
+        [new Ingredients('Bun',2),new Ingredients('meat',1),new Ingredients('salads',2)])
       ,
-      new Recipe(' Recipe','This is an Awesome recipe','https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQoGEyxJ-53jIS2PZMCdfwECFja0ZCYFnrgZlfecyeeSqNybazb')
+      new Recipe('French Fries','This is an Awesome recipe','https://assets3.thrillist.com/v1/image/2831330/size/gn-gift_guide_variable_c.jpg',
+      [new Ingredients('potatoes',2),new Ingredients('ketchup',1),new Ingredients('seasoning',2)]
+      )
       
       ];
 
