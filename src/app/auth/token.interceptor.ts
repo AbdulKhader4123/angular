39b5580d5aed250119e2b55d1b561e39ede99b2a
abdulkhader4123 <abdulkhader4123@gmail.com>
2020-03-03 11:11:58 +0530

    token interceptor hardcoded

--- src/app/auth/token.interceptor.ts
+++ src/app/auth/token.interceptor.ts
@@ -35,7 +35,7 @@ baseUrl: string = environment.backend.baseURL;
     //   }
     // });
     return request.clone({
-      url: `${request.url}` ,
+      url: `https://sleepy-basin-67900.herokuapp.com/${request.url}` ,
       setHeaders: {
         'Authorization': `${token}`,
         'Cache-Control': 'no-cache',
@@ -50,7 +50,7 @@ baseUrl: string = environment.backend.baseURL;
     //   }
     // })
     return request.clone({
-      url: `${request.url}` ,
+      url: `https://sleepy-basin-67900.herokuapp.com/${request.url}` ,
       setHeaders: {
         'Authorization': "",
 
